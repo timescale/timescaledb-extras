bfe912a3159c5cc6dd9630f96aba220cec71e4bd
Keyur Panchal <keyur@timescale.com>
2025-09-08 16:41:13 -0600

    Fix to_timestamp function usage

--- utils/2.22.0-downgrade-cagg-materialization-ranges/02-refresh-saved-ranges.sql
+++ utils/2.22.0-downgrade-cagg-materialization-ranges/02-refresh-saved-ranges.sql
@@ -11,8 +11,8 @@ BEGIN
         JOIN _timescaledb_catalog.continuous_agg ca ON sr.materialization_id = ca.mat_hypertable_id
         ORDER BY sr.materialization_id
     LOOP
-    RAISE NOTICE 'force refreshing %s: start:%s end:%s', range_record.cagg_name, to_timestamp(range_record.lowest/1000000), to_timestamp(range_record.greatest/1000000);
-        CALL refresh_continuous_aggregate(range_record.cagg_name, to_timestamp(range_record.lowest/1000), to_timestamp(range_record.greatest/1000), force => true);
+        RAISE NOTICE 'force refreshing %s: start:%s end:%s', range_record.cagg_name, _timescaledb_functions.to_timestamp(range_record.lowest), _timescaledb_functions.to_timestamp(range_record.greatest);
+        CALL refresh_continuous_aggregate(range_record.cagg_name, _timescaledb_functions.to_timestamp(range_record.lowest), _timescaledb_functions.to_timestamp(range_record.greatest), force => true);
     END LOOP;
 END $$;
 
